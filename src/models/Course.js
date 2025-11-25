c6a38ec8e0b1f7bef8ad935dc54a4cdc95166881
notYAS <yasserfat7alah@gmail.com>
2025-11-25 12:04:30 +0200

    models: gradelevel @ course type identified

--- src/models/Course.js
+++ src/models/Course.js
@@ -41,6 +41,7 @@ const CourseSchema = new mongoose.Schema({
     },
 
     gradeLevel: {
+        type: String,
         enum: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
         required: true
     },
