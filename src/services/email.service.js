1b377a688ce855d66c9739e63d520b7b32708ca6
notYAS <yasserfat7alah@gmail.com>
2025-11-29 22:39:10 +0200

    config: nodemailer setup and email service

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,13 +1,37 @@
+import transporter from "../config/nodemailer.js";
+import { FROM_EMAIL } from "../utils/constants.js";
 
 
 class EmailService {
-    async sendEmail(to, subject, body) {
-        // Implementation for sending email
+    constructor(transporter) {
+        this.transporter = transporter;
+    }
+
+    async sendEmail(to, subject, body, text) {
+         const mailOptions = {
+        from: FROM_EMAIL,
+        to,
+        subject,
+        html: body,
+        text: text || body.replace(/<[^>]*>/g, ""), // Strip HTML for text version
+      };
+
+      const info = await this.transporter.sendMail(mailOptions);
+      return { success: true, messageId: info.messageId };
     }
 
     async initiatePasswordReset(email) {
-        // Implementation for initiating password reset
+        const otp = Math.floor(100000 + Math.random() * 900000); // Generate a 6-digit OTP
+        const subject = "Password Reset Request";
+        const body = `
+            <p>Dear User,</p>
+            <p>You requested a password reset. Here is your OTP:</p>
+            <p>${otp}</p>
+            <p>If you did not request this, please ignore this email.</p>
+            <p>Best regards,<br/>Your App Team</p>
+        `;
+        return await this.sendEmail(email, subject, body);
     }
 }
 
-export default new EmailService();
+export default new EmailService(transporter);
