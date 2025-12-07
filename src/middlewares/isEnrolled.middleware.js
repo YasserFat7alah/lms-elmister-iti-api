9d0a38330d5c2d304e6c16d70b61ba6287e7f865
Ahmed-Ossamaa <aossama2015@gmail.com>
2025-12-07 16:26:05 +0200

    Modified isEnrolled middleware

--- src/middlewares/isEnrolled.middleware.js
+++ src/middlewares/isEnrolled.middleware.js
@@ -1,54 +1,32 @@
 import Enrollment from "../models/Enrollment.js";
+import Assignment from "../models/Assignment.js";
 import AppError from "../utils/app.error.js";
 
-const ACTIVE_STATUSES = ["active", "trialing"];
 
 const isEnrolled = () => {
     return async (req, res, next) => {
 
         const studentId = req.user._id;
+        const { assignmentId } = req.body;
 
-        // groupId from params or body or query
-        const groupId =
-            req.params.groupId ||
-            req.body.groupId ||
-            req.query.groupId;
+        if (!assignmentId)
+            return next(AppError.badRequest("AssignmentId is required"));
 
-        if (!groupId) {
-            return next(
-                AppError.badRequest("groupId is required to check enrollment")
-            );
-        }
+        const assignment = await Assignment.findById(assignmentId);
+        if (!assignment)
+            return next(AppError.notFound("Assignment not found"));
 
-        const enrollment = await Enrollment.findOne({
+        const isEnrolled = await Enrollment.findOne({
             student: studentId,
-            group: groupId,
-            status: { $in: ACTIVE_STATUSES }
-        }).populate("group course");
+            group: assignment.group
+        });
+        if (!isEnrolled)
+            return next(AppError.forbidden("You are not enrolled in this group"));
 
-        if (!enrollment) {
-            return next(
-                AppError.forbidden(
-                    "You are not enrolled in this group or your subscription is inactive"
-                )
-            );
-        }
-        // Checking if trial period has ended (currentPeriodEnd)
-        if (enrollment.status === "trialing" && enrollment.currentPeriodEnd) {
-            const now = new Date();
-            const trialEnd = new Date(enrollment.currentPeriodEnd);
-
-            if (now > trialEnd) {
-                return next(
-                    AppError.forbidden("Your trial period has ended. Please subscribe to continue.")
-                );
-            }
-        }
 
         // Pass it to the next middleware
-        req.enrollment = enrollment;
-        req.course = enrollment.course;
-        req.group = enrollment.group;
+        req.assignment = assignment;
+        req.groupId = assignment.group;
 
         next();
     };
